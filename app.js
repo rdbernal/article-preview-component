43e6b3680251ccbed36ccef1ac4ea9a421571a69
Rodrigo Bernal <rodrigobernal19@gmail.com>
2021-07-21 20:26:14 -0300

    HTML and CSS ready to deploy

---
+++ app.js
@@ -0,0 +1,7 @@
+const shareButton = document.querySelector(".js-share__button__container");
+
+function shareFunction() {
+    console.log("Clicou!!");
+}
+
+shareButton.addEventListener("click", shareFunction);
